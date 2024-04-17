fbaec164c0aa8e8cee0512b48b6eba74fb843b72
ruslan1us <rusikkoliada@gmail.com>
2024-04-17 12:49:51 +0200

    Added default value to year model

--- src/api/services/models.py
+++ src/api/services/models.py
@@ -1,4 +1,5 @@
 from pydantic import Field, BaseModel
+from src.api.time_func import set_year
 
 
 class Month(BaseModel):
@@ -6,7 +7,7 @@ class Month(BaseModel):
 
 
 class Year(BaseModel):
-    year: int = Field(ge=2000, le=2100)
+    year: int = Field(default=set_year(), ge=2000, le=2100)
 
 
 class Day(BaseModel):
--- src/api/time_func.py
+++ src/api/time_func.py
@@ -1,5 +1,9 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 def set_date():
     return datetime.strptime(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
+
+
+def set_year():
+    return date.today().year
