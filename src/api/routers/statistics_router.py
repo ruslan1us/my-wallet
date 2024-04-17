bba76f94b65a1c67b3b26a7f43bad4dc69b73fa3
ruslan1us <rusikkoliada@gmail.com>
2024-04-17 12:28:10 +0200

    Moved endpoints that uses month as an argument to monthly_router

--- src/api/routers/statistics_router.py
+++ src/api/routers/statistics_router.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,7 +7,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.auth.auth import auth_backend
 from src.api.auth.manager import get_user_manager
 from src.api.auth.models import User
-from src.api.services.models import Month, Day
+from src.api.crud_services.crud_monthly import CRUDyear
+from src.api.expense.schemas import ExpenseRead
+from src.api.income.schemas import TipRead, SalaryRead
+from src.api.services.models import Month, Day, Year
 from src.database import get_async_session
 
 from fastapi_users import FastAPIUsers
@@ -25,58 +30,6 @@ fastapi_users = FastAPIUsers[User, int](
 current_user = fastapi_users.current_user()
 
 
-@router.get('/all_expenses_amounts')
-async def all_expenses_amounts(month: Month = Depends(Month),
-                               session: AsyncSession = Depends(get_async_session),
-                               user: User = Depends(current_user),
-                               services: Services = Depends(Services)):
-    try:
-        expenses_amount = await services.get_all_expenses_amounts(month=month, session=session, user=user)
-
-        if expenses_amount == [None]:
-            raise Exception
-
-        return expenses_amount
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-
-@router.get('/all_income_amounts')
-async def all_income_amounts(month: Month = Depends(Month),
-                             session: AsyncSession = Depends(get_async_session),
-                             user: User = Depends(current_user),
-                             services: Services = Depends(Services)):
-    try:
-        income_amounts = await services.get_all_income_amounts(month=month, session=session, user=user)
-
-        return income_amounts
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-
-@router.get('/stats_by_month')
-async def stats_by_month(month: Month = Depends(Month),
-                         session: AsyncSession = Depends(get_async_session),
-                         user: User = Depends(current_user),
-                         services: Services = Depends(Services)):
-    try:
-        expenses_amount = await services.get_all_expenses_amounts(month=month, session=session, user=user)
-        income_amounts = await services.get_all_income_amounts(month=month, session=session, user=user)
-
-        if expenses_amount == [None] and income_amounts != [None]:
-            return income_amounts
-
-        if income_amounts == [None] and expenses_amount != [None]:
-            return expenses_amount
-
-        result = income_amounts[0] - expenses_amount[0]
-
-        return result
-
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-
 @router.get('/all_expenses_amounts_by_day')
 async def all_expenses_amounts_by_day(day: Day = Depends(Day),
                                       month: Month = Depends(Month),
@@ -94,3 +47,53 @@ async def all_expenses_amounts_by_day(day: Day = Depends(Day),
     except Exception:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
+
+@router.get('/year_expense', response_model=List[ExpenseRead])
+async def get_year_expense(year: Year = Depends(Year),
+                           session: AsyncSession = Depends(get_async_session),
+                           user: User = Depends(current_user),
+                           crud_services: CRUDyear = Depends(CRUDyear)):
+    try:
+        expenses = await crud_services.get_year_expense(year=year, session=session, user=user)
+
+        if expenses == []:
+            raise Exception
+
+        return expenses
+
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@router.get('/year_salary', response_model=List[SalaryRead])
+async def get_year_salary(year: Year = Depends(Year),
+                          session: AsyncSession = Depends(get_async_session),
+                          user: User = Depends(current_user),
+                          crud_services: CRUDyear = Depends(CRUDyear)):
+    try:
+        salary = await crud_services.get_year_salary(year=year, session=session, user=user)
+
+        if salary == []:
+            raise Exception
+
+        return salary
+
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@router.get('/year_tip', response_model=List[TipRead])
+async def get_year_tip(year: Year = Depends(Year),
+                       session: AsyncSession = Depends(get_async_session),
+                       user: User = Depends(current_user),
+                       crud_services: CRUDyear = Depends(CRUDyear)):
+    try:
+        tip = await crud_services.get_year_tip(year=year, session=session, user=user)
+
+        if tip == []:
+            raise Exception
+
+        return tip
+
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
